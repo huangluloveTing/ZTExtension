d68a94a31b1bac72f168258b38a8b4d855556037
lu.huang@biz-united.com.cn <>
2019-04-25 15:38:47 +0800

    update

--- ZTExtension/HZExtension/ZTUtil.m
+++ ZTExtension/HZExtension/ZTUtil.m
@@ -12,11 +12,11 @@
 
 + (void) openSystemSetting:(NSString *)sysytemName {
     //iOS8之后
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@""]];
-    //如果App没有添加权限，显示的是设定界面。如果App有添加权限（例如通知），显示的是App的设定界面。
-    //iOS8之前
-    //先添加一个url type如下图，在代码中调用如下代码,即可跳转到设置页面的对应项
-    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=WIFI"]];
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@""]];
+//    //如果App没有添加权限，显示的是设定界面。如果App有添加权限（例如通知），显示的是App的设定界面。
+//    //iOS8之前
+//    //先添加一个url type如下图，在代码中调用如下代码,即可跳转到设置页面的对应项
+//    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=WIFI"]];
 }
 
 @end
